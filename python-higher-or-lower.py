01082cd537c6e18d7ce6bb7b9d7069645bd987c3
Jaredpg <lmp57912000@yahoo.com>
2023-06-19 12:14:59 +0100

    fix: fixing while loop errors

--- python-higher-or-lower.py
+++ python-higher-or-lower.py
@@ -1,27 +1,19 @@
 import random
+from timeit import repeat
 guess_number = random.randint(1, 100)
-
+print("a random number has been generated")
 x = int(input("guess the number generated betweeen 1-100: "))
+
+while x != int(guess_number):
+    if x > int(guess_number):
+        print("your number was higher than the random number")
+        x = int(input("guess again your number was higher: "))
+    
+
+    if x < int(guess_number):
+        print("your number was lower than the random number")
+        x = int(input("guess again your number was lower: "))
+    
 if x == int(guess_number):
-    print("your answer is correct the number is" + guess_number)
-
-elif x >= guess_number:
-    print("your guess a bit higher than the random number")
-    
-
-elif x <= guess_number:
-    print("your guess is a bit lower than the random number")
-    
-while x >= guess_number:
-    print("your guess a bit higher than the random number")
-    x = int(input("guess the number generated betweeen 1-100: "))
-    if x == int(guess_number):
-        print("your answer is correct the number is" + guess_number)
-
-while x <= guess_number:
-    print("your guess is a bit lower than the random number")
-    x = int(input("guess the number generated betweeen 1-100: "))
-    if x == int(guess_number):
-        print("your answer is correct the number is" + guess_number)
-
+    print("your answer is correct the number is " + str(guess_number))
 
