48d5690c5bf837a9efbf1eaf4fd5165a2c57e285
Jaredpg <lmp57912000@yahoo.com>
2023-06-20 12:33:22 +0100

    created python bank class

---
+++ python-bank.py
@@ -0,0 +1,41 @@
+import datetime
+
+class bank_account:
+    def __init__(self, value, interest_rate, compound_interest, year):
+        self.value = value
+        self.year = year
+        self.compound_interest = compound_interest
+        self.interest_rate = interest_rate 
+
+    def interest_rate(self, interest_rate):
+        interest = self.value * 1.3 
+        print(interest_rate)
+
+    def withdraw(self, amount):
+        self.value -= amount
+      
+        print(self.value) 
+
+    def deposit(self, amount):
+
+        self.value += amount
+        print(self.value)
+
+    def total(self): 
+        print(self.value)
+    
+    def increment_year(self):
+        if self.compound_interest:
+            self.value *= self.interest_rate 
+        else:
+            self.value += self.interest_rate
+        self.year += 1
+        print(self.year)
+            
+myBank = bank_account(20, 1.3, False, 2023)
+
+myBank.total()
+myBank.deposit(8)
+myBank.withdraw(5)
+myBank.increment_year()
+myBank.total()
