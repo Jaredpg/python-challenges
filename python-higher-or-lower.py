e3357885f98a9e860a7a5c12ede68fc14d6ab859
Jaredpg <lmp57912000@yahoo.com>
2023-06-20 12:33:00 +0100

    removed uneeded content

--- python-higher-or-lower.py
+++ python-higher-or-lower.py
@@ -1,5 +1,5 @@
 import random
-from timeit import repeat
+# from timeit import repeat
 guess_number = random.randint(1, 100)
 print("a random number has been generated")
 x = int(input("guess the number generated betweeen 1-100: "))
@@ -14,6 +14,5 @@ while x != int(guess_number):
         print("your number was lower than the random number")
         x = int(input("guess again your number was lower: "))
     
-if x == int(guess_number):
-    print("your answer is correct the number is " + str(guess_number))
+print("your answer is correct the number is " + str(guess_number))
 
