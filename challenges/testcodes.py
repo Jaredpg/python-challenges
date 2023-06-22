234f72292f25a378b8b9da27c303e4a1dc99142c
Jaredpg <lmp57912000@yahoo.com>
2023-06-22 09:33:15 +0100

    testing

---
+++ challenges/testcodes.py
@@ -0,0 +1,16 @@
+from flask import Flask  
+from flask import render_template
+    #from flaskwebgui import FlaskUI
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():  
+    return render_template('index.html')
+
+@app.route("/home", methods=['GET'])
+def home(): 
+    return render_template('some_page.html')
+
+#if __name__ == "__main__":
+  #FlaskUI(app=app, server="flask").run()
