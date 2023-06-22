a527993e826d13a8677fa61a78f9c7331a4916b1
Jaredpg <lmp57912000@yahoo.com>
2023-06-22 14:00:35 +0100

    testing file list code

--- challenges/testcodes.py
+++ challenges/testcodes.py
@@ -11,3 +11,13 @@ path = "/"
 
 file_list = os.listdir(path)
 print(file_list)
+
+import time
+def animate(text):
+    for i in range(len(text)):
+        print(text[:i+1])
+        time.sleep(0.1)
+
+
+print("loading") 
+animate("...")
