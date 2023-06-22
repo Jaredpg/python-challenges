d6b6d726bdf355033dec0746590f29ff33d44371
Jaredpg <lmp57912000@yahoo.com>
2023-06-22 15:53:37 +0100

    testing ways to edit file

--- challenges/testcodes.py
+++ challenges/testcodes.py
@@ -1,23 +1,8 @@
 import os
 
-# Get the list of all files and directories
-# in the root directory
-path = "/"
-#dir_list = os.listdir(path)
+# Absolute path of a file
+old_name = ("project-records/" + input(""))
+new_name = ("project-records/" + input(""))
 
-#print("project-records'", path, "' :")
-#print(dir_list)
-# print the list
-
-file_list = os.listdir(path)
-print(file_list)
-
-import time
-def animate(text):
-    for i in range(len(text)):
-        print(text[:i+1])
-        time.sleep(0.1)
-
-
-print("loading") 
-animate("...")
+# Renaming the file
+os.rename(old_name, new_name)
