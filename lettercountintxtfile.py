9465ce633b63e89f779f353e1cd3e53c000a1636
Jaredpg <lmp57912000@yahoo.com>
2023-06-22 09:19:20 +0100

    capital letters and lower case letters aren't seperate anymore

--- lettercountintxtfile.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter, OrderedDict
-
-words = open("words.txt", "r")
-counter = Counter(words.read().lower())
-
-sorted_list = (sorted(counter.most_common()))
-
-[print(i) for i in sorted_list]
