c820ef071391cecd66a10509e8531cf9034cb0e1
Jaredpg <lmp57912000@yahoo.com>
2023-06-23 14:36:22 +0100

    testing ways to deal with errors

--- challenges/testcodes.py
+++ challenges/testcodes.py
@@ -1,8 +1,6 @@
-import os
-
-# Absolute path of a file
-old_name = ("project-records/" + input(""))
-new_name = ("project-records/" + input(""))
-
-# Renaming the file
-os.rename(old_name, new_name)
+try:
+    # Open file in read-only mode
+    with open("not_here.txt", 'r') as f:
+        f.write("Hello World!")
+except IOError as e:
+    print("An error occurred:", e)
